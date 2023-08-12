e0943abb107e25789c99a0436051ecf1d3754edb
Pechinkin Sergey <pechinkinsergey@yandex.ru>
2023-08-12 22:50:19 +0400

    done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix === undefined){
+    return [];
+  }
+  return matrix.reduce((result, element, index) => {
+    if(index % 2 === 0){
+      result.push(...element);
+    } else {
+      result.push(...element.reverse());
+    }
+    return result;
+  },[])
 }
